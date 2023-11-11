5eaa5000a65fbd3ea4c883829c0bf5b1aeee37da
John Bars <john.barsq@gmail.com>
2023-11-11 16:42:51 +0800

    left sidebar setup: modify styling

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -8,10 +8,12 @@ import Image from "next/image";
 import { sidebarLinks } from "@/constants";
 
 const LeftSidebar = () => {
+  // get the current URL's pathname
   const pathname = usePathname();
+  // console.log("pathname: ", pathname);
   return (
-    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-[9rem] max-sm:hidden lg:w-[300]">
-      <div className="flex flex-col gap-6">
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-[9rem] shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266]">
+      <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
@@ -20,18 +22,18 @@ const LeftSidebar = () => {
             <Link
               href={item.route}
               key={item.label}
-              className={`flex items-center justify-start gap-4 p-4 ${
+              className={` ${
                 isActive
                   ? "primary-gradient rounded-lg text-light-900"
                   : "text-dark300_light900"
-              }`}
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
             >
               <Image
                 src={item.imgURL}
                 alt={item.label}
                 width={20}
                 height={20}
-                className={`h-auto w-auto ${isActive ? "" : "invert-colors"}`}
+                className={`${isActive ? "" : "invert-colors"}`}
               />
               <p
                 className={`${
@@ -44,10 +46,11 @@ const LeftSidebar = () => {
           );
         })}
       </div>
+
       <SignedOut>
         <div className="flex flex-col gap-3">
           <Link href="/sign-in">
-            <Button className="btn-secondary w-full rounded-lg shadow-none">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
               <Image
                 src="/assets/icons/account.svg"
                 alt="login"
@@ -59,7 +62,7 @@ const LeftSidebar = () => {
             </Button>
           </Link>
           <Link href="/sign-up">
-            <Button className="btn-tertiary w-full rounded-lg shadow-none">
+            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
               <Image
                 src="/assets/icons/sign-up.svg"
                 alt="login"
@@ -67,9 +70,7 @@ const LeftSidebar = () => {
                 height={20}
                 className="invert-colors h-auto w-auto lg:hidden"
               />
-              <span className="text-dark400_light900 max-lg:hidden">
-                Sign Up
-              </span>
+              <span className="max-lg:hidden">Sign Up</span>
             </Button>
           </Link>
         </div>
