f8797070810f2f70ef2d1ab8b3823e3547152b48
John Bars <john.barsq@gmail.com>
2023-11-13 18:10:02 +0800

    feat: create reusable component NoResult This will be displayed when there's no data available to be shown

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,39 @@
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+import { title } from "process";
+
+const questions = [
+  // {
+  //   _id: "1",
+  //   title:
+  //     "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+  //   tags: [{ _id: "1", name: "next.js" }],
+  //   author: "Sujata",
+  //   upvotes: 55,
+  //   answers: 32,
+  //   views: 2800,
+  //   createdAt: new Date("2023-11-13T12:00:00.000Z"),
+  // },
+  // {
+  //   _id: "2",
+  //   title: "How do I use express as a custom server in NextJS?",
+  //   tags: [
+  //     { _id: "2", name: "next13" },
+  //     { _id: "3", name: "express" },
+  //     { _id: "4", name: "fastify" },
+  //   ],
+  //   author: "Brandon",
+  //   upvotes: 2,
+  //   answers: 3,
+  //   views: 368,
+  //   createdAt: new Date("2023-11-13T13:00:00.000Z"),
+  // },
+];
 
 export default function Home() {
   return (
@@ -34,6 +64,23 @@ export default function Home() {
       </div>
       {/* Visible for large devices */}
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <div key={question._id}>QuestionCard</div>
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! Ask a Question and kickstart the
+            discussion. Our query could be the next big thing others learn from. Get
+            involved!"
+            link="/"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
---
+++ components/shared/NoResult.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+interface NoResultProps {
+  title: string;
+  description: string;
+  link: string;
+  linkTitle: string;
+}
+
+const NoResult = ({ title, description, link, linkTitle }: NoResultProps) => {
+  return (
+    <div className="flex-center mt-10 flex w-full flex-col">
+      {/* Image for light mode */}
+      <Image
+        src="/assets/images/light-illustration.png"
+        alt="No result illustration"
+        width={270}
+        height={270}
+        className="block w-auto object-contain dark:hidden"
+      />
+      {/* Image for dark mode */}
+      <Image
+        src="/assets/images/dark-illustration.png"
+        alt="No result illustration"
+        width={270}
+        height={270}
+        className="hidden w-auto object-contain dark:block"
+      />
+      <h2 className="h2-bold text-dark200_light900 mt-8">{title}</h2>
+      <p className="body-regular text-dark500_light700 my-3.5 max-w-md text-center">
+        {description}
+      </p>
+      <Link href={link}>
+        <Button
+          className="paragraph-medium mt-5 min-h-[36px] rounded-lg bg-primary-500 px-4 py-3 text-light-900 hover:bg-primary-500/90 dark:bg-primary-500/80 
+        dark:text-light-900 dark:hover:bg-primary-500"
+        >
+          {linkTitle}
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default NoResult;
