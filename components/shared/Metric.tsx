f283d703a4cb0cd54896acc4e477990fe254921a
John Bars <john.barsq@gmail.com>
2023-11-14 09:10:33 +0800

    feat: add Metric for the author, answers, views

--- components/shared/Metric.tsx
+++ components/shared/Metric.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface MetricProps {
@@ -20,8 +21,8 @@ const Metric = ({
   textStyles,
   isAuthor,
 }: MetricProps) => {
-  return (
-    <div className="flex-center flex-wrap gap-1">
+  const metricContent = (
+    <>
       <Image
         src={imgUrl}
         width={16}
@@ -30,10 +31,27 @@ const Metric = ({
         className={`object-contain ${href ? "rounded-full" : ""}`}
       />
       <p className={`${textStyles} flex items-center gap-1`}>
-        {value} {title}
+        {value}
+        <span
+          className={`small-regular line-clamp-1 ${
+            isAuthor && "max-sm:hidden"
+          }`}
+        >
+          {title}
+        </span>
       </p>
-    </div>
+    </>
   );
+
+  if (href) {
+    return (
+      <Link href={href} className="flex-center flex-wrap gap-1">
+        {metricContent}
+      </Link>
+    );
+  }
+
+  return <div className="flex-center flex-wrap gap-1">{metricContent}</div>;
 };
 
 export default Metric;
