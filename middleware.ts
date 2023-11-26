fdd866729abceae47b02ffdac52b2ca699b7e014
John Bars <john.barsq@gmail.com>
2023-11-26 22:11:44 +0800

    comment out publicRoutes

--- middleware.ts
+++ middleware.ts
@@ -8,12 +8,12 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/api/webhook",
-    "question/:id",
-    "/tags",
-    "/tags/:id",
-    "/profile/:id",
-    "/community",
-    "/jobs",
+    // "question/:id",
+    // "/tags",
+    // "/tags/:id",
+    // "/profile/:id",
+    // "/community",
+    // "/jobs",
   ],
   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
