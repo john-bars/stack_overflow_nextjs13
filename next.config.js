9c05f2e9be6db1b9d74cd3bcc1274185918d1a19
John Bars <john.barsq@gmail.com>
2023-11-27 18:21:38 +0800

    feat: use the author's picture; allow images from all sources to be displayed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*", // allow images from all sources
+        // hostname: "img.clerk.com", // allow images from clerk
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
