ad5c1586d4b1b2a47524326bbd938ac2410a1f0b
John Bars <john.barsq@gmail.com>
2023-11-27 13:23:21 +0800

    disable eslint camelcase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable camelcase */
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
-// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
 
