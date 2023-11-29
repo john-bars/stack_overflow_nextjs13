3efa8e63fb6c9e55595fda9ed4c4c3bbb95483d0
John Bars <john.barsq@gmail.com>
2023-11-29 20:07:04 +0800

    feat: implement rendering for a specific question by ID - introduce a new action to fetch a question from the database based on its specific ID - create a dedicated '/question/[id]' page for displaying the details of the targeted question - integrate the previously developed Metric component - establish the initial layout of ParseHTML to  render the content or details of the selected question

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,7 +3,11 @@
 import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
 import Tag from "@/database/tag.model";
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import {
+  CreateQuestionParams,
+  GetQuestionByIdParams,
+  GetQuestionsParams,
+} from "./shared.types";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 
@@ -64,5 +68,30 @@ export async function createQuestion(params: CreateQuestionParams) {
   } catch (error) {
     console.error("Error in createQuestion: ", error);
     throw new Error("Failed to create a question.");
+  }
+}
+
+// GET INDIVIDUAL QUESTION
+export async function getQuestionById(params: GetQuestionByIdParams) {
+  try {
+    await connectToDatabase();
+    const { questionId } = params;
+
+    const question = await Question.findById(questionId)
+      .populate({ path: "tags", model: Tag, select: "_id name" })
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id clerkId name picture",
+      });
+
+    if (!question) {
+      throw new Error("Question not found");
+    }
+
+    return question;
+  } catch (error) {
+    console.error("Error in getQuestionById:", error);
+    throw new Error("Failed to get question by ID");
   }
 }
