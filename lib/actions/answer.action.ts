ff314799d830996fa52a3017fe4cffc48d59e41a
John Bars <john.barsq@gmail.com>
2023-12-12 18:12:02 +0800

    feat: integrate filters/sorting for answers

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -39,12 +39,31 @@ export async function createAnswer(params: CreateAnswerParams) {
 export async function getAnswers(params: GetAnswersParams) {
   try {
     connectToDatabase();
-    const { questionId } = params;
+    const { questionId, sortBy } = params;
+
+    let sortOptions = {};
+    switch (sortBy) {
+      case "highestUpvotes":
+        sortOptions = { upvotes: -1 };
+        break;
+      case "lowestUpvotes":
+        sortOptions = { upvotes: 1 };
+        break;
+      case "recent":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "old":
+        sortOptions = { createdAt: 1 };
+        break;
+
+      default:
+        break;
+    }
 
     // Find answers in the database based on the question id
     const answers = await Answer.find({ question: questionId })
       .populate("author", "_id clerkId name picture") // populate the 'author' field with additional data
-      .sort({ createdAt: -1 }); // (newest first)
+      .sort(sortOptions);
 
     return { answers };
   } catch (error) {
