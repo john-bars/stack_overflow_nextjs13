f077e762bdedc1d4a2f0912d97517060e94fae72
John Bars <john.barsq@gmail.com>
2023-12-14 21:58:01 +0800

    feat: Implement global search filters
    - modify the URL by adding the 'type' query with its value
    - dynamically set the 'type' query value in the URL based on the active filter among 'question', 'answer', 'user', and 'tag'

--- components/shared/search/GlobalFilters.tsx
+++ components/shared/search/GlobalFilters.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+"use client";
+import { GlobalSearchFilters } from "@/constants/filters";
+import { formUrlQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 const GlobalFilters = () => {
-  return <div>GlobalFilters</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const typeParams = searchParams.get("type");
+  const [active, setActive] = useState(typeParams || "");
+  const handleTypeClick = (item: string) => {
+    // Check if the clicked item is already active
+    if (active === item) {
+      setActive(""); // if active, deactivate it
+
+      // Reset the 'type' query by setting its value to null
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: null,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      // If clicked item is not active, activate it
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: item.toLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
+  return (
+    <div className="flex items-center gap-5 px-5">
+      <p className="text-dark400_light900 body-medium">Type: </p>
+      <div className="flex gap-3">
+        {GlobalSearchFilters.map((item) => (
+          <button
+            type="button"
+            key={item.value}
+            onClick={() => handleTypeClick(item.value)}
+            className={`light-border-2 small-medium rounded-2xl px-5 py-2 capitalize dark:text-light-800 dark:hover:text-primary-500 ${
+              active === item.value
+                ? "bg-primary-500 text-light-900"
+                : "bg-light-700 text-dark-400 hover:text-primary-500 dark:bg-dark-500"
+            }`}
+          >
+            {item.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default GlobalFilters;
