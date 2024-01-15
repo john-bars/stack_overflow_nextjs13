9fad9ffb80a8997ac89943687446fdcce3a3143f
John Bars <john.barsq@gmail.com>
2024-01-15 21:43:09 +0800

    refactor: improve 'handleDelete' function for questions and answers, add toast notifications for success or error

--- components/shared/EditDeleteAction.tsx
+++ components/shared/EditDeleteAction.tsx
@@ -4,6 +4,7 @@ import { deleteAnswer } from "@/lib/actions/answer.action";
 import { deleteQuestion } from "@/lib/actions/question.action";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
+import { toast } from "../ui/use-toast";
 
 interface Props {
   type: string;
@@ -18,21 +19,61 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
     router.push(`/question/edit/${JSON.parse(itemId)}`);
   };
 
-  const handleDelete = async () => {
-    if (type === "Question") {
-      // Delete a Question
-      await deleteQuestion({
-        questionId: JSON.parse(itemId),
-        path: pathname,
-      });
-    } else if (type === "Answer") {
-      // Delete an Answer
-      await deleteAnswer({
-        answerId: JSON.parse(itemId),
-        path: pathname,
+  // Common function for deleting an entity with confirmation, success, and error handling
+  const deleteEntity = async (
+    deleteFunction: Function,
+    params: any,
+    successTitle: string,
+    errorTitle: string
+  ) => {
+    // Ask for confirmation before deletion
+    const confirmed = window.confirm(`Do you want to delete the ${type}?`);
+
+    if (confirmed) {
+      try {
+        await deleteFunction(params);
+        toast({
+          title: successTitle,
+        });
+      } catch (error: any) {
+        toast({
+          title: errorTitle,
+          description: error.message || "An error occurred during deletion.",
+        });
+      }
+    } else {
+      toast({
+        title: "Deletion canceled",
       });
     }
   };
+
+  const handleDelete = async () => {
+    if (type === "Question") {
+      const params = {
+        questionId: JSON.parse(itemId),
+        path: pathname,
+      };
+      await deleteEntity(
+        deleteQuestion,
+        params,
+        "Question deleted successfully",
+        "Error deleting the question"
+      );
+    } else if (type === "Answer") {
+      const params = {
+        answerId: JSON.parse(itemId),
+        path: pathname,
+      };
+      await deleteEntity(
+        deleteAnswer,
+        params,
+        "Answer deleted successfully",
+        "Error deleting the answer"
+      );
+    }
+  };
+
   return (
     <div className="flex items-center justify-end gap-3 max-sm:w-full">
       {type === "Question" && (
