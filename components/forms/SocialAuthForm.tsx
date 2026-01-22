81f9f998f7c31d0a84792eb185afe678b20475c8
John Bars <john.barsq@gmail.com>
2026-01-22 13:10:51 +0800

    ...

--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -2,24 +2,34 @@
 
 import Image from "next/image";
 import { Button } from "../ui/button";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { toast } from "../ui/use-toast";
+import ROUTES from "@/constants/routes";
 
 const SocialAuthForm = () => {
-  const { data: session } = useSession();
-
   const buttonClass =
     "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5 hover:bg-light-900Hover dark:hover:bg-dark-400Hover transition-colors duration-200";
 
-  const handleSignIn = (provider: "github" | "google") => {
-    signIn(provider);
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: false,
+      });
+    } catch (error) {
+      console.log(error);
+
+      toast({
+        title: "Sign-in Failed",
+        description:
+          error instanceof Error
+            ? error.message
+            : "An error occured during sign-in",
+        variant: "destructive",
+      });
+    }
   };
 
-  if (session) {
-    redirect("/");
-  }
-
-  // If not signed in, show social login buttons
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
       <Button className={buttonClass} onClick={() => handleSignIn("github")}>
